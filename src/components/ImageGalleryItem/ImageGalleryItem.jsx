c97774a54294d190d32280280ebe95e552893ce9
grebenik72 <grebenik72@gmail.com>
2023-09-06 20:50:43 +0300

    final

---
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -0,0 +1,13 @@
+import {
+  ImageGalleryItemStyled,
+  ImageGalleryImg,
+} from './ImageGalleryItem.styled';
+
+
+export const ImageGalleryItem = ({ id, smallUrl, tags, onClickImageItem }) => (
+  <ImageGalleryItemStyled key={id} data-id={id} onClick={onClickImageItem}>
+    <ImageGalleryImg src={smallUrl} alt={tags} data-id={id} />
+  </ImageGalleryItemStyled>
+);
+
+
